c30c0db120bff96c3839947906c70e802d4a163f
chenjie <chenjie@utry.cn>
2018-11-29 12:10:30 +0800

    1

--- src/main/java/com/springcloud/eurekaclient/EurekaClientApplication.java
+++ src/main/java/com/springcloud/eurekaclient/EurekaClientApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class EurekaclientApplication {
+public class EurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaclientApplication.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
 }
